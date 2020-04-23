8b13424c91a84445fe7a2d1908c0a2761e0cf5b3
Tymoteusz Tula <tymoteusz.tula@gmail.com>
2020-04-23 15:22:58 +0100

    implemented gradient of cost function

--- Sources/Sampler/vmc_sampling.cc
+++ Sources/Sampler/vmc_sampling.cc
@@ -260,14 +260,16 @@ Complex C1LocalValue(const AbstractOperator &op, AbstractDensityMatrix &rho,
 /** TTComment:
  * C2 local value used to derive the gradient of cost function
  */
-Complex C2LocalValue(const AbstractOperator &op, AbstractDensityMatrix &rho,
+VectorType C2LocalValue(const AbstractOperator &op, AbstractDensityMatrix &rho,
                      Eigen::Ref<const VectorXd> v, 
                      Eigen::Ref<const VectorXd> gamma)
 {
   int n_vis = rho.Nvisible();  
+  int n_par = rho.Npar();
   Eigen::Ref<const VectorXd> vr = v.head(n_vis);
   Eigen::Ref<const VectorXd> vc = v.tail(n_vis);
   Eigen::VectorXd temp_v;
+  int h = 0;
   
   AbstractOperator::ConnectorsType tochanger;
   AbstractOperator::ConnectorsType tochangec;
@@ -289,43 +291,110 @@ Complex C2LocalValue(const AbstractOperator &op, AbstractDensityMatrix &rho,
   assert(melsr.size() == std::size_t(logvaldiffsr.size()));
   assert(melsc.size() == std::size_t(logvaldiffsc.size()));
   
-  Complex result = 0.0;
+  VectorType result = VectorType::Zero(n_par);
   // Values from hamiltonian part
   for (Index i = 0; i < logvaldiffsr.size(); ++i)
   {
-    result += -I_ * melsr[i] * std::exp(-logvaldiffsr(i));
+    temp_v = v;
+    h = 0;
+    for (auto &el2 : tochanger[i])
+    {
+      temp_v[el2] = newconfr[i][h];
+      ++h;
+    }
+    result += -I_ * melsr[i] * std::exp(-logvaldiffsr(i)) * 
+                      rho.DerLog(temp_v);
   }
   for (Index i = 0; i < logvaldiffsc.size(); ++i)
   {
-    result += I_ * std::conj(melsc[i]) * std::exp(-logvaldiffsc(i));    
+    temp_v = v;
+    h = 0;
+    for (auto &el2 : tochangec[i])
+    {
+      temp_v[el2] = newconfc[i][h];
+      ++h;
+    }  
+    result += I_ * std::conj(melsc[i]) * std::exp(-logvaldiffsc(i)) *
+                    rho.DerLog(temp_v);    
   }
   
   decltype(logvaldiffsr) templogvaldiff;
   AbstractOperator::NewconfsType tempnewconf;
   AbstractOperator::ConnectorsType temptochange;
   // Values from jump operator part (only for spin 1/2)
+  /// TODO: implement jump operator for any system
   for (int j = 0; j < n_vis; ++j)
   {
     if (vc[j] == -1 && vr[j] == -1)
     {
+      temp_v = v;
+      temp_v[j] = 1;
+      temp_v[j + n_vis] = 1;
       tempnewconf = {{1, 1}};
       temptochange = {{j, j + n_vis}};
       templogvaldiff = rho.LogValDiff(v, temptochange, tempnewconf);
-      result += gamma[j] / 2 * std::exp(-templogvaldiff(0));  
+      result += gamma[j] / 2 * std::exp(-templogvaldiff(0)) * 
+                        rho.DerLog(temp_v);  
     }
     if (vr[j] == +1)
     {
-      result -= gamma[j] / 2;  
+      result -= gamma[j] / 2 * rho.DerLog(v);  
     }
     if (vc[j] == +1)
     {
-      result -= gamma[j] / 2;
+      result -= gamma[j] / 2 * rho.DerLog(v);
     }
   }
   
   return result;
 }
 
+/** TTComment:
+ * calculating gradient of cost function for density matrix
+ */
+VectorXcd GradientDM(const Result &result, AbstractDensityMatrix &rho,
+                     const AbstractOperator &op,
+                     Eigen::Ref<const VectorXd> gamma)
+{
+    int n_par = rho.Npar();
+    int n_samples = result.NSamples();
+    
+    Complex locC1_value = 0.0;
+    VectorType locC2_value = VectorType::Zero(n_par);
+    
+    Complex c1_value = 0.0;
+    VectorType o_value = VectorType::Zero(n_par);
+    Complex c2_value = VectorType::Zero(n_par);
+    
+    for (Index i = 0; i < n_samples; ++i)
+    {
+        locC1_value = C1LocalValue(op, rho, result.Sample(i), gamma)
+        locC2_value = C2LocalValue(op, rho, result.Sample(i), gamma)
+        c1_value += pow(abs(locC1_value), 2)
+        o_value += result.LogDerivs()
+        c2_value += conj(locC1_value) * locC2_value
+    }
+    VectorType result = - c1_value * o_value / (n_samples * n_samples) + 
+                          c2_value / n_samples
+    
+    return 2*result.real()
+}
+
+/** TTComment:
+ * calculating expectation values for a given density matrix and 
+ * operator
+ */
+Complex ExpectationDM(const Result &result, AbstractDensityMatrix &rho,
+                      const AbstractOperator &op)
+{
+    int n_samples = result.NSamples();
+    Complex result = 0.0;
+    
+    for (Index i = 0; i < n_samples; ++i)
+    {
+        
+    }
+}
 
 }  // namespace vmc
 }  // namespace netket
